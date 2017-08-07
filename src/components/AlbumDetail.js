7dbc1455e920eba20c6261b6f7646bccf758dd15
Anuja Tupe <anujatupe2012@gmail.com>
2017-08-06 19:07:36 -0700

    Added card and card section component which is used to show the artist name, title and thumbnail. They are reusable components.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({albumRecord}) => {
+
+	const { thumbnail_image, title, artist } = albumRecord;
+	const { textContainerStyle, thumbnailStyle, thumbnailContainerStyle, textStyle } = styles;
+
 	return (
-		<View>
-			<Text>{props.albumRecord.title}</Text>
-		</View>
+		<Card>
+			<CardSection>
+				<View style={thumbnailContainerStyle}>
+					<Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+				</View>
+				<View style={textContainerStyle}>
+					<Text style={textStyle}>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
+			</CardSection>
+		</Card>
 	);
-}
+};
+
+const styles = {
+	textContainerStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+	},
+	textStyle: {
+		fontSize: 18,
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50,
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10,
+	}
+};
 
 export default AlbumDetail;
